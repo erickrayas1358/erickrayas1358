d6bd1863f5121e989f8ab5e2660ea0a4f320a0b4
erickrayas1358 <erick.rayas.312@my.csun.edu>
2025-04-24 02:56:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-target]')
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
                        const target = document.querySelector(tab.dataset.tabTarget)
-  target.classList.add('active')
+  })
+    tab.Contents.forEach(tabContent => tabContent.classList.remove ('active'))
+    target.classList.add('active')
 }}
              }}
                        
